2a124e246ad59e9c3005bcd27f7cee2a95fd1eca
Arno0x <a.github@0x1010.eu>
2015-12-17 09:13:47 +0100

    ClientIP made available as an environment variable

--- runCommand.php
+++ runCommand.php
@@ -69,10 +69,16 @@ if (!$isDynamic) {
 // Note: commands are executed as the user specified in the RUN_AS_USER setting
 else {
 	// Prepare the Gotty command
-	$gottyCommand = "sudo -b -u ".RUN_AS_USER." TERM=".GOTTY_TERM." -i ".GOTTY_PATH." --once -w -p ".GOTTY_TCP_PORT." -a ".GOTTY_BIND_INTERFACE." ".$command." > /dev/null 2>&1";
+	// ** A bit of explanation about the use of the DISPLAY environment **
+	// ** Most SUDOERS environment will have the 'env_reset' option set, especially for untrusted account such as ones used to run the web server.
+	// ** With this option set, the environment variables that can be set by the sudo command is restricted to a small set, including the DISPLAY variable.
+	// ** Because in a TermGate + Gotty typical usage, this variable is most probably unused or, if required, has 90% of chances to point back to the client IP
+	// ** I'm using it to store the client IP, as it can be a useful information once in a Gotty shell.
+	$gottyCommand = "sudo -b -u ".RUN_AS_USER." DISPLAY=".$_SERVER['REMOTE_ADDR']." TERM=".GOTTY_TERM." -i ".GOTTY_PATH." --once -w -p ".GOTTY_TCP_PORT." -a ".GOTTY_BIND_INTERFACE." ".$command." > /dev/null 2>&1";
+
 	
 	// Execute the command
-	exec($gottyCommand, $return);
+	exec($gottyCommand);
 	
 	// DIRTY (!): Wait 500ms, just the time for gotty to be ready to accept incoming connections
 	usleep(500000);
